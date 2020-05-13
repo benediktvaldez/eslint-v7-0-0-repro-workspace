85c12f6dab5090ef8b0331fd4fb4f4d73abb6bcd
Benedikt D. Valdez Stefánsson <bvstefansson@ossur.com>
2020-05-13 09:29:44 +0000

    initial commit

---
+++ apps/repro-app/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'repro-app',
+  preset: '../../jest.config.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/apps/repro-app'
+};
